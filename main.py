2d9a89375ca15e37b64b8bcc95b2743a032c1463
Lukaverik <lukamgreenway@gmail.com>
2022-11-13 14:58:58 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import disnake
+from dotenv import dotenv_values
+import logging
+
+logger = logging.getLogger('disnake')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='disnake.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+token = dotenv_values()["DISCORD_TOKEN"]
+bot = disnake.Client()
+
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+bot.run(token)
